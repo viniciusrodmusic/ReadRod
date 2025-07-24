243928432a020e6bac5cc04f7e7faf6801f1a9ac
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-07-24 18:12:03 -0300

    Avancei na pagina de leitura e coloquei o conteudo dos livros em um json

--- leitura/script.js
+++ leitura/script.js
@@ -1,13 +1,29 @@
-const menuIcon = document.querySelector('.menu-icon')
-const overlay = document.querySelector('.overlay')
+const menuIcon = document.querySelector('.menu-icon') // Menu hambúrguer
+const overlay = document.querySelector('.overlay') // O que aparece quando o menu hambúrguer é pressionado
+const bookContent = document.querySelector('.book-content') // Onde o conteúdo do livro é exibido
+const bookChapter = document.querySelector('.chapter')
+
 // ESCONDE/EXIBE O MENU HAMBÚRGUER
 menuIcon.addEventListener('click', (e) => {
     overlay.classList.toggle('overlay-disable')
 })
 
+let selectedBook = sessionStorage.getItem("livro-selecionado")
+let selectedChapter = 1
 
-const bookContent = document.querySelector('.book-content')
 
+// EXIBINDO O CONTEÚDO NA PÁGINA
 
-console.log(sessionStorage.getItem("livro-selecionado"))
+async function exibirConteudo(selectedBook) {
+    // BUSCANDO O CONTEÚDO DO LIVRO
+   const response = await fetch("../contents/contents.json");
+    // CONVERTENDO PRA OBJETO JAVASCRIPT
+    const responseObject = await response.json();
 
+    return responseObject
+}
+
+exibirConteudo(selectedBook).then( (responseObject) => {
+    bookContent.innerHTML = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["conteudo"]
+    bookChapter.innerText = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["titulo"]
+})
