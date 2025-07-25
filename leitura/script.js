6fb162a80485c90439ea0c34b71c42574e281b21
viniciusrodmusic <aviniciusrodrigu@gmail.com>
2025-07-25 20:03:19 -0300

    Coloquei os botões de troca de página e avancei com o arquivo json

--- leitura/script.js
+++ leitura/script.js
@@ -3,6 +3,9 @@ const overlay = document.querySelector('.overlay') // O que aparece quando o men
 const bookContent = document.querySelector('.book-content') // Onde o conteúdo do livro é exibido
 const bookChapter = document.querySelector('.chapter')
 
+const previousBookButton = document.getElementById('previous-book-button') // BOTÃO LEFT
+const nextBookButton = document.getElementById('next-book-button') // BOTÃO RIGHT
+
 // ESCONDE/EXIBE O MENU HAMBÚRGUER
 menuIcon.addEventListener('click', (e) => {
     overlay.classList.toggle('overlay-disable')
@@ -12,18 +15,52 @@ let selectedBook = sessionStorage.getItem("livro-selecionado")
 let selectedChapter = 1
 
 
-// EXIBINDO O CONTEÚDO NA PÁGINA
 
-async function exibirConteudo(selectedBook) {
+
+
+
+// EXIBINDO O CONTEÚDO NA PÁGINA----------------------
+async function buscarConteudo() {
     // BUSCANDO O CONTEÚDO DO LIVRO
    const response = await fetch("../contents/contents.json");
     // CONVERTENDO PRA OBJETO JAVASCRIPT
     const responseObject = await response.json();
-
+    // RETORNA O OBJETO PARA SER USADO NO THEN
     return responseObject
 }
 
-exibirConteudo(selectedBook).then( (responseObject) => {
-    bookContent.innerHTML = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["conteudo"]
-    bookChapter.innerText = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["titulo"]
+function colocandoConteudo() {
+    buscarConteudo().then( (responseObject) => {
+        bookContent.innerHTML = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["conteudo"]
+        bookChapter.innerText = responseObject[selectedBook - 1]["capitulo"][selectedChapter]["titulo"]
+        console.log("Capítulos disponíveis nesse livro: ", responseObject[selectedBook - 1]["capitulos-disponiveis"])
+        // ARMAZENANDO A QUANT. DE CAPÍTULOS NO SESSION STORAGE DO NAVEGADOR
+        sessionStorage.setItem("availableChapters", responseObject[selectedBook - 1]["capitulos-disponiveis"])
+        })
+        
+    }
+
+colocandoConteudo()
+// PEGA A QUANTIDADE DE CAPÍTULOS DISPONÍVEIS NO LIVRO, QUE ESTÁ ARMAZENADO NO SESSION STORAGE DO NAVEGADOR, E A ARMAZENA
+const availableChapters = sessionStorage.getItem("availableChapters")
+//------------------------------------------------------
+
+
+
+
+
+
+
+
+// ADICIONANDO EVENTO AOS BOTÕES LEFT/RIGHT, PARA QUE O LEITOR POSSA TROCAR AS PÁGINAS
+nextBookButton.addEventListener('click', (event) => {
+    if (selectedChapter < availableChapters) {
+        selectedChapter++
+        colocandoConteudo()
+        console.log(selectedChapter)
+    }
+   
 })
+//------------------------------------------------------------------------------------
+
+
